fe8be45bd1b73c5e609e3ecfbff9079d0ed9a8cb
SergeyKKKoryagin <96138113+SergeyKKKoryagin@users.noreply.github.com>
2024-09-21 09:58:39 +0300

    Add files via upload

---
+++ lanus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Получаем список всех подключений
+connections=$(nmcli -t -f NAME connection show)
+
+# Удаляем каждое подключение, обернув его в кавычки для корректной обработки пробелов и кириллицы
+while IFS= read -r conn; do
+  echo "Удаление подключения: \"$conn\""
+  nmcli connection delete "$conn"
+done <<< "$connections"
+
+echo "Все подключения были удалены."
+
+MAC_ADDRESS=$(nmcli -g GENERAL.HWADDR device show eth0 | sed 's/\\//g')
+nmcli connection add type ethernet con-name Проводное\ соединение\ 1 ifname eth0 ip4 172.16.16.1/24 802-3-ethernet.mac-address $MAC_ADDRESS
