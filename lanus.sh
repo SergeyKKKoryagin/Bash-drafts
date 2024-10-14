ff5eaf1603c18885885bda47a4421a2cce8c1f9f
SergeyKKKoryagin <96138113+SergeyKKKoryagin@users.noreply.github.com>
2024-10-14 10:49:27 +0300

    added double lan variant

--- lanus.sh
+++ lanus.sh
@@ -11,5 +11,7 @@ done <<< "$connections"
 
 echo "Все подключения были удалены."
 
-MAC_ADDRESS=$(nmcli -g GENERAL.HWADDR device show eth0 | sed 's/\\//g')
-nmcli connection add type ethernet con-name Проводное\ соединение\ 1 ifname eth0 ip4 172.16.16.1/24 802-3-ethernet.mac-address $MAC_ADDRESS
+MAC_ADDRESS0=$(nmcli -g GENERAL.HWADDR device show eth0 | sed 's/\\//g')
+MAC_ADDRESS1=$(nmcli -g GENERAL.HWADDR device show eth1 | sed 's/\\//g')
+nmcli connection add type ethernet con-name Проводное\ соединение\ 1 ifname eth0 ip4 172.16.16.1/24 802-3-ethernet.mac-address $MAC_ADDRESS0
+nmcli connection add type ethernet con-name Проводное\ соединение\ 2 ifname eth1 ip4 172.16.16.1/24 802-3-ethernet.mac-address $MAC_ADDRESS1
